675d5d7edcd251431edc088960a303fd21562276
mustur <lorenzo.moriconi1998@gmail.com>
2024-04-08 10:49:47 +0200

    auto-commit

--- srcs/operations.c
+++ srcs/operations.c
@@ -1,1 +1,26 @@
+#include "push_swap.h"
 
+void	sa(t_push stacks)
+{
+
+}
+
+void	sb(t_push stacks)
+{
+
+}
+
+void	ss(t_push stacks)
+{
+
+}
+
+void	pa(t_push stacks)
+{
+
+}
+
+void	pb(t_push stacks)
+{
+	
+}
---
+++ srcs/operations2.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+void	ra(t_push stacks, int revflag)
+{
+
+}
+
+void	rb(t_push stacks, int revflag)
+{
+
+}
+
+void	rr(t_push stacks, int revflag)
+{
+
+}
+
